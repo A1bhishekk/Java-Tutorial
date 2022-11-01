d8b11217228e87019049bd40e33e4c77cd55ce4e
Abhishek Kumar <sahniak56@gmail.com>
2022-11-02 00:37:33 +0530

    java

--- Loops/For_Loop.java
+++ Loops/For_Loop.java
@@ -2,8 +2,8 @@ package Loops;
 
 public class For_Loop {
     public static void main(String[] args) {
-        for(int a=1;a<=100;a++){
-            System.out.println(a);
+        for(int a=1;a<=108;a++){
+            System.out.println("Jai Shree Ram :"+a);
         }
     }
     
--- Taking_Input/Input.java
+++ Taking_Input/Input.java
@@ -1,14 +1,14 @@
 package Taking_Input;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Input {
     public static void main(String[] args) {
         Scanner ip = new Scanner(System.in);
         System.out.print("Enter Your Name : ");
         String name = ip.nextLine();
-        System.out.println(name);
+        System.out.print("Enter Your Age : ");
+        int age = ip.nextInt();
+        System.out.println("Your Name is "+name+" and your age is "+age);
     }
-    
-    
 }
